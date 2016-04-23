9da8d4bcfb0dcefee940b0084664dc4f0b9e3977
Dmitriy Loktev <unknownliveid@hotmail.com>
2016-04-23 14:01:12 +0300

    Add jsc crate which wraps JavaScriptCore

--- ReactLinux/react-gtk-rs/src/lib.rs
+++ ReactLinux/react-gtk-rs/src/lib.rs
@@ -1,6 +1,18 @@
+pub mod bridge;
+
 #[cfg(test)]
 mod tests {
+    use bridge::Bridge;
+
     #[test]
-    fn it_works() {
+    fn bridge_could_be_initialized() {
+        let batched_bridge = Bridge::init("sourceURL".to_string());
+        assert_eq!(batched_bridge.source_url, "sourceURL");
     }
+
+    // #[test]
+    // fn bridge_could_execute_some_js() {
+    //     let batched_bridge = Bridge::init("sourceURL".to_string());
+    //     assert_eq!(batched_bridge.source_url, "sourceURL");
+    // }
 }
