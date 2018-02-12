ff45ab136d0b0c9b45403da0949b7100dcb89c2d
Valentin Shergin <shergin@fb.com>
2018-02-12 00:04:16 -0800

    Reimagining of RCTShadowView layout API
    Summary:
    This is reimagining of interoperability layer between Yoga and ShadowViews (at least in Yoga -> RN part).
    Goals:
     * Make it clear and easy.
     * Make clear separation between "what layout what", now parent always layout children, noone layout itself.
     * Make possible to interleave Yoga layout with custom imperative layout (may be used in SafeAreaView, Text, Modal, InputAccessoryView and so on).
    
    Reviewed By: mmmulani
    
    Differential Revision: D6863654
    
    fbshipit-source-id: 5a6a933874f121d46f744aab99a31ae42ddd4a1b

--- React/Views/RCTShadowView+Layout.m
+++ React/Views/RCTShadowView+Layout.m
@@ -11,31 +11,7 @@
 
 #import <yoga/Yoga.h>
 
-/**
- * Yoga and CoreGraphics have different opinions about how "infinity" value
- * should be represented.
- * Yoga uses `NAN` which requires additional effort to compare all those values,
- * whereas GoreGraphics uses `GFLOAT_MAX` which can be easyly compared with
- * standard `==` operator.
- */
-
-float RCTYogaFloatFromCoreGraphicsFloat(CGFloat value)
-{
-  if (value == CGFLOAT_MAX || isnan(value) || isinf(value)) {
-    return YGUndefined;
-  }
-
-  return value;
-}
-
-CGFloat RCTCoreGraphicsFloatFromYogaFloat(float value)
-{
-  if (value == YGUndefined || isnan(value) || isinf(value)) {
-    return CGFLOAT_MAX;
-  }
-
-  return value;
-}
+#import "RCTAssert.h"
 
 @implementation RCTShadowView (Layout)
 
@@ -76,56 +52,14 @@ CGFloat RCTCoreGraphicsFloatFromYogaFloat(float value)
   };
 }
 
-static inline CGRect NSEdgeInsetsInsetRect(CGRect rect, NSEdgeInsets insets) {
-  rect.origin.x    += insets.left;
-  rect.origin.y    += insets.top;
-  rect.size.width  -= (insets.left + insets.right);
-  rect.size.height -= (insets.top  + insets.bottom);
-  return rect;
-}
-
 - (NSSize)availableSize
 {
-  return  NSEdgeInsetsInsetRect((CGRect){CGPointZero, self.frame.size}, self.compoundInsets).size;
+  return self.layoutMetrics.contentFrame.size;
 }
 
 - (CGRect)contentFrame
 {
-  CGRect bounds = (CGRect){CGPointZero, self.frame.size};
-  return NSEdgeInsetsInsetRect(bounds, self.compoundInsets);
-}
-
-#pragma mark - Measuring
-
-- (CGSize)sizeThatFitsMinimumSize:(CGSize)minimumSize maximumSize:(CGSize)maximumSize
-{
-  YGNodeRef clonnedYogaNode = YGNodeClone(self.yogaNode);
-  YGNodeRef constraintYogaNode = YGNodeNewWithConfig([[self class] yogaConfig]);
-
-  YGNodeInsertChild(constraintYogaNode, clonnedYogaNode, 0);
-
-  YGNodeStyleSetMinWidth(constraintYogaNode, RCTYogaFloatFromCoreGraphicsFloat(minimumSize.width));
-  YGNodeStyleSetMinHeight(constraintYogaNode, RCTYogaFloatFromCoreGraphicsFloat(minimumSize.height));
-  YGNodeStyleSetMaxWidth(constraintYogaNode, RCTYogaFloatFromCoreGraphicsFloat(maximumSize.width));
-  YGNodeStyleSetMaxHeight(constraintYogaNode, RCTYogaFloatFromCoreGraphicsFloat(maximumSize.height));
-
-  YGNodeCalculateLayout(
-    constraintYogaNode,
-    YGUndefined,
-    YGUndefined,
-    self.layoutDirection
-  );
-
-  CGSize measuredSize = (CGSize){
-    RCTCoreGraphicsFloatFromYogaFloat(YGNodeLayoutGetWidth(constraintYogaNode)),
-    RCTCoreGraphicsFloatFromYogaFloat(YGNodeLayoutGetHeight(constraintYogaNode)),
-  };
-
-  YGNodeRemoveChild(constraintYogaNode, clonnedYogaNode);
-  YGNodeFree(constraintYogaNode);
-  YGNodeFree(clonnedYogaNode);
-
-  return measuredSize;
+  return self.layoutMetrics.contentFrame;
 }
 
 #pragma mark - Dirty Propagation Control
