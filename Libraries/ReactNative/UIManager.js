3e2557061a0beff3bdf27d7f082f2676cfa132f2
Russell Ianniello <russell.ianniello@aaf.edu.au>
2018-08-05 21:17:54 +1000

    Modify UIMananger for macos support
    This allows dispatch to native elements.

--- Libraries/ReactNative/UIManager.js
+++ Libraries/ReactNative/UIManager.js
@@ -44,7 +44,7 @@ UIManager.takeSnapshot = function() {
  * only needed for iOS, which puts the constants in the ViewManager
  * namespace instead of UIManager, unlike Android.
  */
-if (Platform.OS === 'ios') {
+if (Platform.OS === 'ios' || Platform.OS === 'macos') {
   Object.keys(UIManager).forEach(viewName => {
     const viewConfig = UIManager[viewName];
     if (viewConfig.Manager) {
